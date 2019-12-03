c9e6c57671a4236cd0d6a782d25fd57b82ea73cd
PrathamRawat <prathamrawat815@gmail.com>
2019-12-03 00:30:44 -0500

    Add file to add random users to the database

---
+++ randomUsers.py
@@ -0,0 +1,7 @@
+from util.user import User
+import names
+import random as r
+
+for x in range(50):
+    date = str(r.randint(2000, 2019)) + "-" + str(r.randint(1, 12)) + "-" + str(r.randint(1, 28))
+    User.new_user(x , "", names.get_full_name(), "Male", "Both", date, "dummy email", "dummy phone number", "lorem ipsum shut up", "yeet street")
