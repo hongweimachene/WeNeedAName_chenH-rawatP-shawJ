2e318743b39b142d437d8d492f3945428af87e63
jjshawshaw <jshaw00@stuy.edu>
2019-12-04 03:11:24 -0500

    fix

--- randomUsers.py
+++ randomUsers.py
@@ -3,10 +3,6 @@ import names
 import random as r
 
 '''Creates a bunch of dummy accounts to store in database'''
-for x in range(50):
-    date = str(r.randint(2000, 2019)) + "-" + str(r.randint(1, 12)) + "-" + str(r.randint(1, 28))
-    User.new_user(x , "", names.get_full_name(), "Male", "Both", date, "dummy email", "dummy phone number", "lorem ipsum shut up", "yeet street")
-
 
 def populate():
     for x in range(50):
@@ -17,4 +13,3 @@ def populate():
         #for i in range(10):
         #    username += r.choice(string.ascii_lowercase)
         User.new_user(x, "", names.get_full_name(gender=gen.lower()), gen, r.choice(["Both", "Males", "Females"]), date, "dummy email", "dummy phone number", "lorem ipsum shut up", f"{r.uniform(35, 45)},{r.uniform(-80, 70)}")
-
