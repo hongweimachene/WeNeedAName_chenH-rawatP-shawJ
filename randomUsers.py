081a3749af9a4b58cd5aa9516869e8f1210b1014
hongweimachene <hchen00@stuy.edu>
2019-12-04 02:34:42 -0500

    update

--- randomUsers.py
+++ randomUsers.py
@@ -2,6 +2,7 @@ from util.user import User
 import names
 import random as r
 
+'''Creates a bunch of dummy accounts to store in database'''
 for x in range(50):
     date = str(r.randint(2000, 2019)) + "-" + str(r.randint(1, 12)) + "-" + str(r.randint(1, 28))
     User.new_user(x , "", names.get_full_name(), "Male", "Both", date, "dummy email", "dummy phone number", "lorem ipsum shut up", "yeet street")
